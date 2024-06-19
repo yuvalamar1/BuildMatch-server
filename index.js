be1e579aee9fe149d7ca51258ab33f7bbe833b18
yuval <yuvalamar15@gmail.com>
2024-06-19 15:45:44 +0300

    route added

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 // Basic route to handle GET requests to the root URL
 app.get("/", (req, res) => {
-  res.send("this is buildmatch backend");
+  res.send("this is buildmatch backendd");
 });
 
 // Another route to handle GET requests to /about
