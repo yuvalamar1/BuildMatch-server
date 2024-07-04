01257091dd3d1137f5e7cba9f3627186bd0872b3
linsadon <93978915+linsadon@users.noreply.github.com>
2024-07-04 19:16:37 +0300

    feat: Add getAllProjects route

--- controllers/projects.js
+++ controllers/projects.js
@@ -40,3 +40,12 @@ export const createproject = async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 };
+
+export const getAllProjects = async (req, res) => {
+  try {
+    const projects = await Project.find();
+    res.status(200).json(projects);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
--- routes/projects.js
+++ routes/projects.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { createproject } from "../controllers/projects.js";
+import { createproject, getAllProjects } from "../controllers/projects.js";
 
 const projectrouter = express.Router();
 
 projectrouter.post("/createproject", createproject);
+projectrouter.get("/getallprojects", getAllProjects);
 
 export default projectrouter;
