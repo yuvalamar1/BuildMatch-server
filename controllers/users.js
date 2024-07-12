8825ced937dcf3ed676727bd1a2cdd57e60eec82
yuval <yuvalamar15@gmail.com>
2024-07-12 15:27:19 +0300

    chore: Update user update logic to handle different user types and password changes

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,7 @@
 import Client from "../models/Client.js";
 import Administrator from "../models/Administrator.js";
 import bcrypt from "bcrypt";
+import express from "express";
 
 export const getuser = async (req, res) => {
   try {
@@ -13,6 +14,49 @@ export const getuser = async (req, res) => {
       const user = await Administrator.findOne({
         _id: userid,
       });
+      res.status(200).json(user);
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+export const updateuser = async (req, res) => {
+  try {
+    const { userid, usertype, userdata } = req.body;
+    let previususer = null;
+    if (usertype === "client") {
+      let user = null;
+      previususer = await Client.findOne({ _id: userid });
+      if (userdata.password === previususer.password) {
+        user = await Client.findOneAndUpdate({ _id: userid }, userdata, {
+          new: true,
+        });
+      }
+      if (userdata.password !== previususer.password) {
+        const salt = await bcrypt.genSalt();
+        userdata.password = await bcrypt.hash(userdata.password, salt);
+        user = await Client.findOneAndUpdate({ _id: userid }, userdata, {
+          new: true,
+        });
+      }
+      res.status(200).json(user);
+    }
+    if (usertype === "administrator") {
+      let user = null;
+      previususer = await Administrator.findOne({ _id: userid });
+      if (userdata.password === previususer.password) {
+        user = await Administrator.findOneAndUpdate({ _id: userid }, userdata, {
+          new: true,
+        });
+      }
+      if (userdata.password !== previususer.password) {
+        const salt = await bcrypt.genSalt();
+        userdata.password = await bcrypt.hash(userdata.password, salt);
+        user = await Administrator.findOneAndUpdate({ _id: userid }, userdata, {
+          new: true,
+        });
+      }
       res.status(200).json(user);
     }
   } catch (err) {
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getuser } from "../controllers/users.js";
+import { getuser, updateuser } from "../controllers/users.js";
 
 const userrouter = express.Router();
 
 userrouter.get("/getuser", getuser);
+userrouter.put("/updateuser", updateuser);
 
 export default userrouter;
