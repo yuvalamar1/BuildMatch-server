bfcb97e2b75d1c4a7951078e0ee3a17668f97894
yuval <yuvalamar15@gmail.com>
2024-09-03 21:58:35 +0300

    refactor: Update getprojetpreferences function to use correct parameter name

--- controllers/preferencelist.js
+++ controllers/preferencelist.js
@@ -3,9 +3,9 @@ import Project from "../models/Project.js";
 
 /* get all preferences */
 export const getprojetpreferences = async (req, res) => {
-  const { projectId, clientId } = req.params;
+  const { projectId, clientid } = req.params;
   try {
-    const preferences = await PreferenceList.findOne({ projectId, clientId });
+    const preferences = await PreferenceList.findOne({ projectId, clientid });
     if (!preferences) {
       return res.status(404).json({ message: "Preferences not found" });
     }
@@ -36,17 +36,24 @@ export const createpreference = async (req, res) => {
   try {
     const { clientid, projectId, preferences } = req.body;
 
+    const existingPreference = await PreferenceList.findOne({
+      clientid,
+      projectId,
+    });
+
     // Find the document and update it, or create a new one if it doesn't exist
     const updatedPreference = await PreferenceList.findOneAndUpdate(
       { clientid, projectId }, // Query to find the document
       { preferences }, // Data to update
       { new: true, upsert: true } // Options: new=true returns the updated document, upsert=true creates the document if it doesn't exist
     );
-    const tmp = await Project.findById(projectId);
-    const updatedAvailablePlaces = tmp.availablePlaces - 1;
-    await Project.findByIdAndUpdate(projectId, {
-      availablePlaces: updatedAvailablePlaces,
-    });
+    if (!existingPreference) {
+      const tmp = await Project.findById(projectId);
+      const updatedAvailablePlaces = tmp.availablePlaces - 1;
+      await Project.findByIdAndUpdate(projectId, {
+        availablePlaces: updatedAvailablePlaces,
+      });
+    }
     res.status(200).json(updatedPreference);
   } catch (err) {
     res.status(500).json({ error: err.message });
